79a665e31c8169f86798d90d983c15ff04276b76
Alex Shafer <alex.shafer@circleci.com>
2020-06-24 10:51:36 -0600

    replace url logic

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function maybeClose(tabId, changeInfo, tab) {
-	if (tab.url.match(/https:\/\/([a-z]+).zoom.us\/[a-z]\/([0-9]+)\?(.*)?status=success/)) {
+	if (tab.url.match(/https:\/\/([a-z]+).zoom.us\/(?:postattendee|j)/) && (tab.url.includes('/postattendeee/') || tab.url.includes('status=success') || tab.url.includes('#success'))) {
 		setTimeout(function() {
 			browser.tabs.remove(tab.id);
 		}, 2000);
