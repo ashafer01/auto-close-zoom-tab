819b58f772c0ad488b2230303d4f26b4220fa5d0
Alex Shafer <alex.shafer@circleci.com>
2020-06-24 10:52:44 -0600

    typo

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 function maybeClose(tabId, changeInfo, tab) {
-	if (tab.url.match(/https:\/\/([a-z]+).zoom.us\/(?:postattendee|j)/) && (tab.url.includes('/postattendeee/') || tab.url.includes('status=success') || tab.url.includes('#success'))) {
+	if (tab.url.match(/https:\/\/([a-z]+).zoom.us\/(?:postattendee|j)/) && (tab.url.includes('/postattendee/') || tab.url.includes('status=success') || tab.url.includes('#success'))) {
 		setTimeout(function() {
 			browser.tabs.remove(tab.id);
 		}, 2000);
